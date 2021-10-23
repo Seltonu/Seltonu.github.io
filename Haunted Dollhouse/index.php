a3e722df3c3f848e03207ee0c69c7ad1994bf34d
Steven Gutierrez <steven.s.gutier@gmail.com>
2021-10-23 00:27:28 -0700

    Haunted Dollhousee 1st milestone

---
+++ Haunted Dollhouse/index.php
@@ -0,0 +1,74 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dani's World</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/8a90f91dac.js" crossorigin="anonymous"></script>
+</head>
+
+<body style="background-color: #f8aff2;">
+    <style>
+        .border,.border-bottom {
+            border-color: #d470a2 !important;
+        }
+    </style>
+
+    <div class="container-fluid">
+        <!-- Header -->
+        <div class="row border-bottom border-5">
+            <div class="col-2"></div>
+            <div class="col-8 d-flex align-items-center justify-content-center">
+                <i class="fas fa-heart fa-3x me-3"></i>
+                <img style="width: 80%" src="Assets/banner.png" alt="">
+                <i class="fas fa-heart fa-3x me-3"></i>
+            </div>
+            <!-- <div class="col-2"></div>
+            <div class="d-flex align-items-center justify-content-center border-bottom">
+
+            </div> -->
+        </div>
+        <div class="row mt-3">
+            <div class="col-2">
+                <img style="width: 100%;" src="Assets/art_button.png" alt="Art Button"></img>
+            </div>
+            <div class="col-8">
+            </div>
+            <div class="col-2">
+                <img style="width: 100%;" src="Assets/blog_button.png" alt="Blog Button"></img>
+            </div>
+        </div>
+        <!-- Draft area -->
+        <div class="row mt-3">
+            <div class="col-2"></div>
+            <div class="col-8 d-flex justify-content-center align-center">
+                <div class="align-center">
+                    <img class="card-img-top border border-5" src="$" alt="Card image cap">
+                </div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+        <!-- Comic Navigation -->
+        <div class="row mt-3 mb-5">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <i class="fas fa-arrow-left me-4 fa-2x"></i>
+                <i class="fas fa-arrow-right fa-2x me-2"></i>
+                <b>Go to &nbsp&nbsp</b><input style="width: 8%;" class="form-text" type="text" placeholder="420" aria-label="default input example"></input>
+            </div>
+            <div class="col-2"></div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+            </div>
+            <div class="col-auto">
+                Message me: dani@gmail.com
+            </div>
+        </div>
+    </div>
+    <div class="d-flex align-items-bottom justify-content-end">
+    </div>
+</body>
+</html>
